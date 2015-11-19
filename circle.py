92701c3b78c4be5da6bd3344cb37ff509489281e
Nate Olsen <nso4wg@virginia.edu>
2015-11-18 20:57:32 -0500

    Added pics code needs reworking.

---
+++ circle.py
@@ -0,0 +1,25 @@
+import runWorld as rw
+import drawWorld as dw
+import pygame as pg
+import turtle
+from random import randint
+
+name = "Random Circle"
+width = 800
+height = 800
+rw.newDisplay(width, height, name)
+
+
+def circle (centerpoint, radius)
+    radius = randint(0,20)
+    turtle.up()
+    turtle.setpos(x,y)
+    turtle.done()
+    turtle.circle(radius)
+    print("The Radius is")
+(2.0 * 3.14 * radius)
+
+circle((400,400),radius)
+    
+    
+    
--- soccer.py
+++ soccer.py
@@ -1,47 +1,38 @@
 import runWorld as rw
 import drawWorld as dw
 import pygame as pg
+import turtle
 from random import randint
 
-# Initialize world
-name = "Cat Spiral"
+name = "Soccer Game"
 width = 800
 height = 800
 rw.newDisplay(width, height, name)
 
-myimage = dw.loadImage("cat.bmp")
+myimage = dw.loadImage("soccer_ball_1.bmp")
+#goal = dw.loadImage("goal.bmp")
 
 def updateDisplay(state):
     dw.fill(dw.white)
-    dw.draw(myimage, (state[0], state[1],state[2]))
+    dw.draw(myimage, (state[0], state[1], state[2], state[3]))
+    #dw.draw(goal, (0, 0, 0, 0))
 
-# state -> state
 def updateState(state):
-    def spiral(n, angle, step):
-        for step in range(n):
-            forward(step)
-            left (angle)
-    spiral(140,61,10)
-    state = spiral
-    return(state)
+    return(state[0]+state[2],state[1] + state[3],state[2], state[3])
 
-# state -> bool
 def endState(state):
     if (state[0] > width or state[0] < 0 or state[1] > height or state[1] < 0):
         return True
     else:
         return False
 
-# state -> event -> state
 def handleEvent(state, event):  
-
     if (event.type == pg.MOUSEBUTTONDOWN):
-        return(state[0],state[1],randint(-5,5),randint(-5,5))
+        print("You Win")
     else:
-        return(state)
-
-initState = (400, 400)
-
+        print("You Lose")
+   
+initState = (400, 700, randint(-2,2),randint(-7,-3))
 frameRate = 60
 
 rw.runWorld(initState, updateDisplay, updateState, handleEvent,
