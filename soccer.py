b38c15bfb823d04a564efd4461d25c1585c3d025
Nate Olsen <nso4wg@virginia.edu>
2015-12-04 12:20:10 -0500

    Made the project object oriented

--- soccer.py
+++ soccer.py
@@ -11,28 +11,43 @@ rw.newDisplay(width, height, name)
 myimage = dw.loadImage("soccer_ball_1.bmp")
 goal = dw.loadImage("goal.bmp")
 
+class State:
+    xpos = 400
+    ypos = 700
+    xvel = randint(-2,2)
+    yvel = randint(-12,-10)
+    def move(self):
+        self.xpos = self.xpos + self.xvel
+        self.ypos = self.ypos + self.yvel
+    
+initState = State()
+
 def updateDisplay(state):
     dw.fill(dw.white)
     dw.draw(goal, (0, 0, 0, 0))
-    dw.draw(myimage, (state[0], state[1], state[2], state[3]))
+    dw.draw(myimage, (state.xpos, state.ypos, state.xvel, state.yvel))
 
 def updateState(state):
-    return(state[0]+state[2],state[1] + state[3],state[2], state[3])
+    state.move()
+    return (state)
 
 def endState(state):
-    if ((state[0] > width or state[0] < 0 or state[1] > height or state[1] < 0) or (state[2]==0 and state[3]==0)):
+    if ((state.xpos > width or state.xpos < 0 or state.ypos > height or state.ypos < 0) or (state.xvel==0 and state.yvel==0)):
         return True
     else:
         return False
 
 def handleEvent(state, event):
     p = pg.mouse.get_pos()
-    if ((p[0]>=state[0] and p[0] <= (state[0] + 98)) and  ((p[1]>=state[1] and p[1] <= (state[1] + 100)))):
-        return(state[0], state[1], 0, 0)
+    if ((p[0]>=state.xpos and p[0] <= (state.xpos + 98)) and  ((p[1]>=state.ypos and p[1] <= (state.ypos + 100)))):
+        state.xvel = 0
+        state.yvel = 0
+        return(state)
     else:
         return(state)
    
-initState = (400, 700, randint(-2,2),randint(-12,-10))
+#initState = (400, 700, randint(-2,2),randint(-12,-10))
+
 frameRate = 60
 
 rw.runWorld(initState, updateDisplay, updateState, handleEvent, endState, frameRate)
